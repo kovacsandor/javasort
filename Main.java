d5d2eed1182a4c3e0e2c21ccaedcb101818d4f5a
kovacsandor <kovacs.andor@rocketmail.com>
2016-10-19 15:44:37 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,58 @@
+package test20161019;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ArrayList<Type> types = new ArrayList<>();
+		for (int i = 0; i < 100; i++) {
+			int randomNumber1 = randomNumber();
+			int randomNumber2 = randomNumber();
+			types.add(new Type(randomNumber1, randomNumber2, randomNumber1 + " " + randomNumber2));
+		}
+
+		for (Type type : types) {
+
+			System.out.println(type.param1 + " " + type.param2 + " " + type.param3);
+		}
+
+		System.out.println("-------------");
+
+		Collections.sort(types, new Comparator<Type>() {
+
+			@Override
+			public int compare(Type t1, Type t2) {
+
+				return (int) t1.param1 < t2.param1 ? -1 : 1;
+			}
+		});
+
+		for (Type type : types) {
+
+			System.out.println(type.param1 + " " + type.param2 + " " + type.param3);
+		}
+	}
+
+	public static int randomNumber() {
+
+		return (int) Math.floor(Math.random() * 100) + 1;
+	}
+
+	public static class Type {
+
+		int param1;
+		int param2;
+		String param3;
+
+		Type(int param1, int param2, String param3) {
+
+			this.param1 = param1;
+			this.param2 = param2;
+			this.param3 = param3;
+		}
+	}
+}
